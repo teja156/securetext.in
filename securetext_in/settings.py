c45dbeb2de5a5d79bd145170f0273cefd62f7d2a
teja156 <tejaswaroop156@gmail.com>
2020-03-21 16:26:09 +0530

    intrduce postgres db

--- securetext_in/settings.py
+++ securetext_in/settings.py
@@ -76,12 +76,21 @@ WSGI_APPLICATION = 'securetext_in.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+''' Development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+
+#Deploy Database
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
